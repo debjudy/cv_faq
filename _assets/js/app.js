24d390a78652bb1b9b051780244b3efd09cb5c6c
Joe Krzystan <joseph.krzystan@gsa.gov>
2020-03-20 09:59:41 -0400

    limit action to load more button

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -30,10 +30,10 @@ if (content) {
   wrapper.classList.remove('display-none');
 
   document.addEventListener('click', function (event) {
+    if (!event.target.hasAttribute('load-questions')) return;
     var content = document.querySelector('#' + event.target.getAttribute('aria-controls'));
-
-      event.target.setAttribute('aria-expanded', true);
-      content.classList.remove('display-none');
-      wrapper.classList.add('display-none');
+    event.target.setAttribute('aria-expanded', true);
+    content.classList.remove('display-none');
+    wrapper.classList.add('display-none');
   });
 }
